31bbca9dc857537ec51bc9fc1bcf080fb3cf9997
Deepa Arya <126440385+Aarya1107@users.noreply.github.com>
2024-01-25 12:54:48 +0530

    Update UpcomingCourse.js

--- src/Components/HomePage01/Body/UpcomingCourse.js
+++ src/Components/HomePage01/Body/UpcomingCourse.js
@@ -17,7 +17,7 @@ export default function UpcomingCourse() {
   const scrollRef = useRef(null);
 
   useEffect(() => {
-    fetch("http://http://13.200.156.92:8000/api/courses")
+    fetch("http://13.200.156.92:8000/api/courses")
       .then((response) => response.json())
       .then((data) => {
         console.log("Fetched data:", data);
