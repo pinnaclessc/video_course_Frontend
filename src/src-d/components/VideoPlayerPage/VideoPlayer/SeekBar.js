6f853807516c61a3119693b48649b0c01e1d8bab
Deepa Arya <126440385+Aarya1107@users.noreply.github.com>
2024-01-27 13:22:39 +0530

    Update SeekBar.js

--- src/src-d/components/VideoPlayerPage/VideoPlayer/SeekBar.js
+++ src/src-d/components/VideoPlayerPage/VideoPlayer/SeekBar.js
@@ -1,36 +1,33 @@
-// SeekBar.js
-import React, { useRef, useEffect } from "react"
-import styles from "./SeekBar.module.css" // Import the CSS module
+import React, { useRef, useEffect } from "react";
+import styles from "./SeekBar.module.css";
 
 const SeekBar = ({ currentTime, duration, onSeek }) => {
-  const seekbarRef = useRef(null)
+  const seekbarRef = useRef(null);
 
   useEffect(() => {
-    const seekbar = seekbarRef.current
+    const seekbar = seekbarRef.current;
 
     const handleSeek = (e) => {
-      const seekTime = e.target.value
-      onSeek(parseFloat(seekTime))
-    }
+      const seekTime = e.target.value;
+      onSeek(parseFloat(seekTime));
+    };
 
-    seekbar.addEventListener("input", handleSeek)
+    seekbar.addEventListener("input", handleSeek);
 
     return () => {
-      seekbar.removeEventListener("input", handleSeek)
-    }
-  }, [onSeek])
+      seekbar.removeEventListener("input", handleSeek);
+    };
+  }, [onSeek]);
 
   const calculateProgress = () => {
     if (duration > 0) {
-      return (currentTime / duration) * 100
+      return (currentTime / duration) * 100;
     }
-    return 0
-  }
+    return 0;
+  };
 
   return (
     <div className={styles.seekbar}>
-      {" "}
-      {/* Use the CSS module class */}
       <input
         type="range"
         ref={seekbarRef}
@@ -39,10 +36,14 @@ const SeekBar = ({ currentTime, duration, onSeek }) => {
         step="0.1"
         value={currentTime}
         onChange={() => {}}
+        className={styles.rangeInput}
       />
-      <div style={{ width: `${calculateProgress()}%` }}></div>
+      <div
+        style={{ width: `${calculateProgress()}%` }}
+        className={styles.progress}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
-export default SeekBar
+export default SeekBar;
