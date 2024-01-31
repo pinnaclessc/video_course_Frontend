1d00d160f0303c67287a4e184c64d8c821a7a8af
Aarya1177 <deepaarya@ssccglpinnacle.com>
2024-01-31 12:00:00 +0530

    issue resolved

--- src/Components/HomePage01/SignUpPage/SignUpPage.js
+++ src/Components/HomePage01/SignUpPage/SignUpPage.js
@@ -1,6 +1,6 @@
+import { Link, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import classes from "./SignUpPage.module.css";
-import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUpPage() {
   const storedUser = localStorage.getItem("user");
