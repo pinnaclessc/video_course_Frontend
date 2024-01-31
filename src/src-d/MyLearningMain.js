0ca917b6910aa390972a8f9c895e438d934b5fb1
Aarya1177 <deepaarya@ssccglpinnacle.com>
2024-01-31 14:28:06 +0530

    updated

--- src/src-d/MyLearningMain.js
+++ src/src-d/MyLearningMain.js
@@ -2,12 +2,18 @@ import React from "react"
 import MyLearning from "./components/MyLearningPage/MyLearning"
 import FilterComponents from "./components/BodyContent/FilteringSection/FilterComponent"
 import MainContent from "./components/BodyContent/Main Content/MainContent"
+import styles from "./MyLearningMain.module.css"
+import { useParams } from "react-router"
+
 function MyLearningMain() {
+  const { userId, course_id } = useParams();
   return (
     <>
-      <MyLearning />
+    <div className={styles.main_container}>
+    <MyLearning />
       <FilterComponents />
-      <MainContent/>
+      <MainContent userId={userId} course_id={course_id} />
+      </div>
     </>
   )
 }
